cbb5a7db44929087557e31afdd3423293e071236
ikgoomophuting-del <ikgoomophuting@gmail.com>
2025-09-20 14:04:12 +0200

    Create settings.py

---
+++ messaging_app/messaging_app/settings.py
@@ -0,0 +1,19 @@
+django-admin startproject messaging_app
+cd messaging_app
+
+pip install djangorestframework
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    # Third-party
+    'rest_framework',
+
+    # Local apps
+    'chats',
+]
