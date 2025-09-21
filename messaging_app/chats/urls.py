ab838cb8c1e66007e9f348b1f4b92784c9d7ecdd
ikgoomophuting-del <ikgoomophuting@gmail.com>
2025-09-21 14:10:02 +0200

    Update urls.py

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from .views import ConversationViewSet, MessageViewSet
 
 # Create a router and register our viewsets
-router = DefaultRouter()
+routes = DefaultRouter()
 router.register(r"conversations", ConversationViewSet, basename="conversation")
 router.register(r"messages", MessageViewSet, basename="message")
 
