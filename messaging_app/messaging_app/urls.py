9958da516ad03a9827370b3537dace69e0efecf1
ikgoomophuting-del <ikgoomophuting@gmail.com>
2025-09-21 14:45:14 +0200

    Update urls.py

--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -1,8 +1,9 @@
+# messaging_app/urls.py
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("admin/", admin.site.urls),   # Django admin
-    path("api/", include("chats.urls")),  # Include chats app routes
+    path('admin/', admin.site.urls),
+    path('api/', include('chats.urls')),         # conversations + nested messages
+    path('api-auth/', include('rest_framework.urls')),  # for login/logout in DRF browsable API
 ]
-
